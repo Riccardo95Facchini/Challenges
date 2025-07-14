52111b18fbadd9e1586d42239bfedd3ddf9807ee
Riccardo Facchini <riccardo.facchini@flowe.com>
2025-07-14 17:25:00 +0200

    Daily challenge

---
+++ Solved/GetDecimalValueClass.cs
@@ -0,0 +1,16 @@
+﻿namespace Problems.Solved;
+
+public class GetDecimalValueClass
+{
+    public int GetDecimalValue(ListNode head)
+    {
+        int result = 0;
+        while (head != null)
+        {
+            result = result * 2 + head.val;
+            head = head.next;
+        }
+
+        return result;
+    }
+}
