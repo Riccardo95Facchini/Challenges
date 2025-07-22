8f2e70dfdb691fa89664abe2cc7d0c7255880188
Riccardo Facchini <riccardo95facchini@gmail.com>
2025-07-22 22:00:10 +0200

    Palindrome number

---
+++ Solved/PalindromeNumber.cs
@@ -0,0 +1,24 @@
+﻿namespace Problems.Solved;
+
+public class PalindromeNumber
+{
+    // https://leetcode.com/problems/palindrome-number/description/
+    // Given an integer x, return true if x is a palindrome, and false otherwise.
+
+    public bool Run(int x)
+    {
+        if (x < 0)
+            return false;
+
+        int temp = x;
+        int reversed = 0;
+
+        while (temp != 0)
+        {
+            reversed = (reversed * 10) + temp % 10;
+            temp /= 10;
+        }
+
+        return reversed == x;
+    }
+}
